6f796bb60bd92fed2d5abe3ff30230bd52f250b9
Eric Beutler <e_beutler@u.pacific.edu>
2017-03-02 12:26:47 -0800

    Changed icon, added basic app bar and drawer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,85 @@
-import React from 'react'
-import { IndexLink, Link } from 'react-router'
-import './Header.scss'
+import React, { Component } from 'react';
+import { IndexLink, Link } from 'react-router';
+import { browserHistory } from 'react-router';
+import AppBar from 'material-ui/AppBar';
+import LeftNav from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import { blueA700 } from 'material-ui/styles/colors'
 
-export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
-  </div>
-)
+export class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+        };
+
+    }
+
+    dispatchNewRoute(route) {
+        // Used in LeftNav onClick to route to different pages
+        browserHistory.push(route);
+        this.setState({
+            open: false,
+        });
+
+    }
+
+    handleClickOutside() {
+        // Used to close LeftNav on click outside of it
+        this.setState({
+            open: false,
+        });
+    }
+
+    openNav() {
+        // Used to open LeftNav when the left menu button is clicked
+        this.setState({
+            open: true,
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <LeftNav
+                    open={this.state.open}
+                    onRequestChange={() => this.handleClickOutside()}
+                    docked={false}
+                >
+                    {
+                        <div>
+                            <MenuItem
+                                onClick={() => this.dispatchNewRoute('/')}
+                                style={{
+                                    textAlign: 'left',
+                                }}
+                            >
+                                Home
+                            </MenuItem>
+
+                            <MenuItem
+                                onClick={() => this.dispatchNewRoute('/counter')}
+                                style={{
+                                    textAlign: 'left',
+                                }}
+                            >
+                                Counter
+                            </MenuItem>
+                        </div>
+                    }
+                </LeftNav>
+                <AppBar
+                    title="The Watch List"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onLeftIconButtonTouchTap={() => this.openNav()}
+                    style={{
+                        backgroundColor: blueA700,
+                        textAlign: 'left',
+                    }}
+                />
+            </div>
+        );
+    }
+}
 
 export default Header
