b857445b7c757df284a2bccaa89db9d3665c3cea
Eric Beutler <e_beutler@u.pacific.edu>
2017-03-10 12:58:15 -0800

    TVmaze script now supports getting episodes based on the season number provided

--- TVmaze/tvmaze.py
+++ TVmaze/tvmaze.py
@@ -2,10 +2,11 @@
 from tvmaze_args import TVmazeArgs
 from tvmaze_search import TVmazeSearch
 from tvmaze_show_details import TVmazeShowDetails
+from tvmaze_episodes import TVMazeEpisodes
 
 
 def main():
-    """Main file for TVmaze script
+    """Main function for TVmaze script
 
     Arguments: N/A
 
@@ -14,18 +15,29 @@ def main():
                              results based on title provided
         getDetailsJSON():    if details option provided, JSON containing
                              details related to the ID provided
+        getEpisodeJSON():    if episodes option provided, JSON containing
+                             episodes for season provided
     """
     args = TVmazeArgs()
 
+    # search functionality
     if(args.getSearch()):
         searchShows = TVmazeSearch(args.getShowTitle(), args.getDebug())
         searchShows.search()
         return searchShows.getResultJSON()
 
+    # show details functionality
     if(args.getDetails()):
         showDetails = TVmazeShowDetails(args.getShowID(), args.getDebug())
         showDetails.getShowDetails()
         return showDetails.getDetailsJSON()
+
+    # episode functionality
+    if(args.getEpisodeSeason()):
+        episodes = TVMazeEpisodes(args.getShowID(), args.getEpisodeSeason(),
+                                  args.getDebug())
+        episodes.getEpisodes()
+        return episodes.getEpisodeJSON()
 
 
 if __name__ == '__main__':
