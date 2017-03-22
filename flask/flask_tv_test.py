e386fbe31cb1f482967992f135454c2eb62a7683
Eric Beutler <e_beutler@u.pacific.edu>
2017-03-21 18:12:38 -0700

    Added tests for search and details

--- flask/flask_tv_test.py
+++ flask/flask_tv_test.py
@@ -3,6 +3,21 @@
 import pytest
 import requests
 
+status_ok = 200
+type_json = "application/json"
+
+
+@pytest.fixture
+def search_request():
+    """Request for search JSON (ex. The Office)"""
+    return requests.get("http://localhost:5000/search/the+office")
+
+
+@pytest.fixture
+def details_request():
+    """Request for details JSON (ex. The Office)"""
+    return requests.get("http://localhost:5000/details/526")
+
 
 @pytest.fixture
 def trending_request():
@@ -10,18 +25,60 @@ def trending_request():
     return requests.get("http://localhost:5000/trending")
 
 
+def test_search_status(search_request):
+    """Checks that search status code is 200 (OK)"""
+    assert search_request.status_code == status_ok
+
+
+def test_search_content(search_request):
+    """Checks that search Content-Type is application/json"""
+    assert search_request.headers["Content-Type"] == type_json
+
+
+def test_search_keys(search_request):
+    """Checks that search JSON has correct keys"""
+    searchKeys = ["id", "imdbRating", "poster", "title", "year"]
+    data = search_request.json()
+    show = data[0]
+
+    for key in searchKeys:
+        if key not in show:
+            assert key in show
+
+
+def test_details_status(details_request):
+    """Checks that details status code is 200 (OK)"""
+    assert details_request.status_code == status_ok
+
+
+def test_details_content(details_request):
+    """Checks that details Content-Type is application/json"""
+    assert details_request.headers["Content-Type"] == type_json
+
+
+def test_details_keys(details_request):
+    """Checks that details JSON has correct keys"""
+    detailsKeys = ["cast", "id", "imdbRating", "network", "numSeasons",
+                   "poster", "summary", "title", "year"]
+    data = details_request.json()
+
+    for item in detailsKeys:
+        if item not in data:
+            assert item in data
+
+
 def test_trending_status(trending_request):
-    """Checks that status code is 200 (OK)"""
-    assert trending_request.status_code == 200
+    """Checks that trending status code is 200 (OK)"""
+    assert trending_request.status_code == status_ok
 
 
 def test_trending_content(trending_request):
-    """Checks that Content-Type is application/json"""
-    assert trending_request.headers["Content-Type"] == "application/json"
+    """Checks that trending Content-Type is application/json"""
+    assert trending_request.headers["Content-Type"] == type_json
 
 
 def test_trending_keys(trending_request):
-    """Checks that JSON has correct keys"""
+    """Checks that trending JSON has correct keys"""
     trendingKeys = ["id", "imdbRating", "poster", "title", "year"]
     data = trending_request.json()
     show = data[0]
