a5a9b4ed0c5dfd6d7c02a5b74017236a97ed3632
Eric Beutler <e_beutler@u.pacific.edu>
2017-04-04 19:03:12 -0700

    Updated styling of calendar

--- src/routes/Calendar/calendar.js
+++ src/routes/Calendar/calendar.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './calendar.css';
 
 BigCalendar.momentLocalizer(moment);
 const events = [
@@ -10,24 +10,28 @@ const events = [
         'title': 'Legion',
         'start': new Date(2017, 2, 29, 22, 0, 0),
         'end': new Date(2017, 2, 29, 23, 0, 0),
-        'desc': 'Airs on FX at 11pm'
+        'desc': 'Airs on FX at 10pm'
     },
 ]
 
 export default class CalendarPage extends Component {
-render () {
+    render () {
         return (
             <div
                 id="calendar-page"
                 style={{
-                    padding:35,
-                    height: 1000,
+                    paddingTop: 50,
+                    paddingBottom: 50,
+                    paddingLeft: 200,
+                    paddingRight: 200,
+                    height: 800,
                 }}
             >
                 <BigCalendar
                     selectable
                     events={events}
                     onSelectEvent={event => alert(event.desc)}
+                    scrollToTime={new Date(1970, 1, 1, 12)}
                 />
             </div>
         );
