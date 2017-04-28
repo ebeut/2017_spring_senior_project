52935a5fb4bcf69351c8246a0630ddbd6d19925b
Navjot Singh <navjotsingh1996@aol.com>
2017-04-28 09:13:19 -0700

    fixing login, I hope
    Signed-off-by: Navjot Singh <navjotsingh1996@aol.com>

--- src/routes/Login/login-reducer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { login, register } from './action';
-import LoginPage from './login';
-
-const mapDispatchToProps = { login, register };
-
-const mapStateToProps = (state) => ({ userData: state.userData });
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginPage)
