32bef2fe27eef5415ddb84cde5f77c0c60ebe35b
Eric Beutler <e_beutler@u.pacific.edu>
2017-05-02 11:25:35 -0700

    Started favorites page

--- src/routes/Favorites/fav-reducer.js
+++ src/routes/Favorites/fav-reducer.js
@@ -2,9 +2,10 @@ import { connect } from 'react-redux';
 import Favorite from './favorite';
 import { getFav } from './action';
 import { isLogin, logout } from '../login/action';
+import { getShowInfo } from '../Show-Info/action';
 
-const mapDispatchToProps = { getFav, isLogin, logout };
+const mapDispatchToProps = { getFav, isLogin, logout, getShowInfo };
 
-const mapStateToProps = (state) => ({ favInfo: state.favInfo, userData: state.userData });
+const mapStateToProps = (state) => ({ favInfo: state.favInfo, userData: state.userData, showData: state.showInfo });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Favorite)
--- src/routes/Favorites/favorite.js
+++ src/routes/Favorites/favorite.js
@@ -1,7 +1,16 @@
 import React, { Component, PropTypes } from 'react';
+import {List, ListItem} from 'material-ui/List';
+import Checkbox from 'material-ui/Checkbox';
+import Divider from 'material-ui/Divider';
+import FavoriteHeart from 'material-ui/svg-icons/action/favorite';
+import FavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+import { pinkA200 } from 'material-ui/styles/colors';
 import Header from '../../components/Header';
 import LogoutDlg from '../../components/LogoutDlg';
 import Loading from '../../components/Loading';
+
+// dictionary for favorites checklist
+var favTitles = []
 
 export default class Favorite extends Component {
 
@@ -37,9 +46,59 @@ export default class Favorite extends Component {
       } else {
         const userName = newProps.userData.loginData;
         this.setState({userName});
+        this.props.getFav(newProps.userData.loginData)
       }
     }
+
+    if(this.props.showData !== newProps.showData && newProps.showData) {
+        if(newProps.showData.show.id) {
+            // information to be saved from show details request
+            var temp = {
+                'id': newProps.showData.show.id,
+                'title': newProps.showData.show.title,
+                'summary': newProps.showData.show.summary,
+            }
+            console.log(temp)
+            favTitles.push(temp)
+        }
+    }
+
+    if(this.props.favInfo.favorites != newProps.favInfo.favorites) {
+        if(newProps.favInfo.favorites) {
+            for(var i = 0; i < newProps.favInfo.favorites.length; i++) {
+                var show = newProps.favInfo.favorites[i]
+                this.props.getShowInfo(show)
+            }
+        }
+    }
   }
+
+mkFavList() {
+    const favs = (
+        <List>
+            { favTitles.map((fav) => (
+                <div key={fav.id} style={{height: '100%', cursor: 'pointer'}}>
+                    <ListItem
+                        style={{height: '100%', cursor: 'pointer'}}
+                        primaryText={`${fav.title}`}
+                        secondaryText={<div style={{height: '100%', overflow: 'visible', whiteSpace: 'normal'}}>{`${fav.summary}`}</div>}
+                        leftCheckbox= {
+                            <Checkbox
+                              checkedIcon={< FavoriteHeart />}
+                              uncheckedIcon={< FavoriteBorder />}
+                              iconStyle={{fill: pinkA200}}
+                              defaultChecked={true}
+                            />
+                        }
+                    />
+                    <Divider />
+                </div>
+            ))}
+        </List>
+    );
+    return favs
+}
+
   render () {
 
     return (
@@ -47,7 +106,15 @@ export default class Favorite extends Component {
         <LogoutDlg open={this.state.logout} email={this.state.userName} />
         <Header userEmail={this.state.userName ? this.state.userName : ''} logout={this.props.logout} />
         <Loading id="favorite-loading" open={this.state.open} />
-      </div>
+
+        <h4 style={{textAlign: 'center', paddingTop: 25}}>
+            Favorites
+        </h4>
+
+        <div style={{ width: '75%', margin: '0 auto' }}>
+            {this.mkFavList()}
+        </div>
+    </div>
     );
   }
 }
